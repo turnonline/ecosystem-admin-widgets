3ca6ff21703b66af296053b36933a03f27a9b90f
Jozef Pohorelec <pohorelec@comvai.com>
2019-09-21 22:21:30 +0200

    added blue background to pricing detail pricing summary section #TECO-141

--- src/main/java/biz/turnonline/ecosystem/widget/shared/ui/SectionTitle.java
+++ src/main/java/biz/turnonline/ecosystem/widget/shared/ui/SectionTitle.java
@@ -44,4 +44,9 @@ public class SectionTitle
     {
         this.title.setDescription( title );
     }
+
+    public void setIconMarginLeft( double marginLeft )
+    {
+        icon.setMarginLeft( marginLeft );
+    }
 }
